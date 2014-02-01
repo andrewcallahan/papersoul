5c2a9bb140870570c2ed5611417609c78be04ee1
Andrew Callahan <andrew.callahan@gmail.com>
2014-01-31 23:40:43 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Papersoul::Application.config.secret_key_base = '29b51e97658204783ce03df09cc65ddf1473c4d13675921aa2bebc0d0954aec2478a13c42be9e6c3fc5679b7102b2cda17f45667ebb7be9de9c5f340311377bf'
